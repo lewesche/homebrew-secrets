4c42cc04383c38317d248fda2d6426275c3a510e
Leif Wesche <wesche@usc.edu>
2021-01-26 00:28:30 -0800

    new version v0.1.1

--- secrets.rb
+++ secrets.rb
@@ -1,8 +1,8 @@
 class Secrets < Formula
   desc "Tool for storing and managing encrypted text, like passwords or whatever else"
   homepage "https://github.com/lewesche/secrets"
-  url "https://github.com/lewesche/secrets/releases/download/v0.1.0/secrets-mac.tar.gz"
-  sha256 "29944417baf7c3a2a8ead6855f27c4a6e550aa873531827f2d2e7198a0e63cc0"
+  url "https://github.com/lewesche/secrets/releases/download/v0.1.1/secrets-mac.tar.gz"
+  sha256 "d71a5fe2c4012de4192917adcc74ebab531cf5e3356801778d943c56c4d4c846"
 
   def install
     bin.install "secrets"
